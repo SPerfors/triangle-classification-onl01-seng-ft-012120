9a82af52bcf5be5cd4c3e2dbe25d2d732189af85
Steven Perfors <stevecanbuild@gmail.com>
2020-02-12 18:13:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,16 @@ require 'pry'
 class Triangle
   #attr_accessor :triangle, :side, :equilateral, :isosceles, :scalene
   
-  def initialize()
-    @name = name 
-    @triangle = triangle
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
   end
+  
+  def kind()
+    if (@a <= 0 || @b<= 0 || @c <= 0)
+      raise TriangleError
+    elsif 
   
   class TriangleError < StandardError
   end
