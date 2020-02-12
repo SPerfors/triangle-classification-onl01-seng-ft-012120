05fda1305988986ab5b7f378ffc0ed4910b6f23b
Steven Perfors <stevecanbuild@gmail.com>
2020-02-12 18:15:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,10 @@ class Triangle
   def kind()
     if (@a <= 0 || @b<= 0 || @c <= 0)
       raise TriangleError
-    elsif 
+    elsif (@a + @b <= @c || @a + @c <= @b || @b + @c <= @a)
+      raise TriangleError 
+    else 
+      if 
   
   class TriangleError < StandardError
   end
